44438501a7584a8c605bbafc04a1c9d559b2e4e3
vitorjpmelo <vitorjeronimomelo@gmail.com>
2022-06-28 17:25:08 -0300

    Ajustado erros no projeto biblioteca

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,6 +22,7 @@ namespace Biblioteca.Controllers
         public IActionResult Index()
         {
             Autenticacao.CheckLogin(this);
+            ViewData["mensagem"] = "Seja bem vindo ao sistema de controle de emprestimo de livros";
             return View();
         }
 
@@ -33,16 +34,14 @@ namespace Biblioteca.Controllers
         [HttpPost]
         public IActionResult Login(string login, string senha)
         {
-            if(login != "admin" || senha != "123")
-            {
+
+            if (Autenticacao.verificaLoginSenha(login,senha,this)) {
+                return RedirectToAction("Index");
+            } else {
                 ViewData["Erro"] = "Senha inválida";
                 return View();
-            }
-            else
-            {
-                HttpContext.Session.SetString("user", "admin");
-                return RedirectToAction("Index");
-            }
+            } 
+           
         }
 
         public IActionResult Privacy()
